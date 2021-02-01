9e6ce58a8891a816e8544e7f9326e824459bdcea
Malaz Alkoj <malaz.alkoj@nav.no>
2021-02-01 12:58:15 +0100

    WIP skriv ut knappen justeringer foregår

--- src/felleskomponenter/Layout/Layout.tsx
+++ src/felleskomponenter/Layout/Layout.tsx
@@ -6,6 +6,9 @@ import { DecoratorParts } from '../../utils/dekorator';
 import { DecoratorEnv } from '../decorator/DecoratorEnv';
 import './Layout.less';
 import { CookiesProvider } from 'react-cookie';
+import { useRef } from 'react';
+import logEvent from '../../amplitude/amplitude';
+import ReactToPrint from 'react-to-print';
 
 export const Layout = (props: {
     title: string;
@@ -14,6 +17,9 @@ export const Layout = (props: {
     decoratorParts?: DecoratorParts;
     children: React.ReactChild[];
 }) => {
+    const panelRef = useRef<HTMLDivElement>(null);
+    const lastNedKnappRef = useRef<HTMLButtonElement>(null);
+
     return (
         <div className="layout">
             <Head>
@@ -37,7 +43,26 @@ export const Layout = (props: {
                         iconUrl={props.bannerIconUrl === undefined ? '' : props.bannerIconUrl}
                     />
                     <div className="layout__wrapper">
-                        <div className="layout__content">{props.children}</div>
+                        <div className="layout__content"  ref={panelRef}>
+                                <ReactToPrint
+                                    onBeforePrint={() => logEvent('forside barnehage', 'print')}
+                                    onAfterPrint={() => {
+                                        if (lastNedKnappRef.current) {
+                                            lastNedKnappRef.current.focus();
+                                        }
+                                    }}
+                                    content={() => panelRef.current}
+                                    trigger={() => (
+                                        <button
+                                            ref={lastNedKnappRef}
+                                            className="sammenligningspanel-barnehage__knapp knapp"
+                                        >
+                                            Last ned
+                                        </button>
+                                    )}
+                                />
+                                {props.children}
+                        </div>
                     </div>
                 </CookiesProvider>
             </div>
