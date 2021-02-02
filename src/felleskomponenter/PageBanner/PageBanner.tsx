2a0b45010258705e909f465a2c6155659be20491
Malaz Alkoj <malaz.alkoj@nav.no>
2021-02-02 16:20:47 +0100

    legge til kontekst i banner med styling

--- src/felleskomponenter/PageBanner/PageBanner.tsx
+++ src/felleskomponenter/PageBanner/PageBanner.tsx
@@ -5,13 +5,15 @@ export const PageBanner = (props: { isFrontPage: boolean; title: string; iconUrl
     return (
         <div className="page-banner">
             <div className="page-banner__innhold">
-                <Sidetittel tag="h1" className="page-banner__tekst">
-                    {props.title}
-                </Sidetittel>
-                <Normaltekst>
-                    Du får hjelp til å gjennomføre samtaler med medarbeiderne og bruke erfaringene
-                    til forebyggende arbeid.
-                </Normaltekst>
+                <div className="page-banner__tekst-og-kontekst">
+                    <Sidetittel tag="h1" className="page-banner__tekst">
+                        {props.title}
+                    </Sidetittel>
+                    <Normaltekst className="page-banner__kontekst-tekst">
+                        Du får hjelp til å gjennomføre samtaler med medarbeiderne og bruke
+                        erfaringene til forebyggende arbeid
+                    </Normaltekst>
+                </div>
             </div>
         </div>
     );
