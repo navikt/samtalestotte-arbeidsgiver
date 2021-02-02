18719be01406291748a06de29d33dd9fcc34caf2
Malaz Alkoj <malaz.alkoj@nav.no>
2021-02-02 14:24:50 +0100

    legge til språk til samtalestøtte (#25)
    * legge til språk til samtalestøtte
    
    * sette bransjsen til testmiljøet

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const withTranspileModules = nextTranspileModules(navFrontendModuler);
 module.exports = withTranspileModules(
     withLess({
         basePath: '/samtalestotte',
+        i18n: {
+            locales: ['no'],
+            defaultLocale: 'no',
+        },
         target: 'server',
         trailingSlash: false,
         reactStrictMode: true,
