804b017bc503aebdcb229826c440b9f5a2088a38
Thomas Dufourd <thomas.dufourd@gmail.com>
2021-03-19 09:56:17 +0100

    946 sende mottatt ia tjeneste til ia-tjeneste-metrikker (#63)
    * starte med å lage ny ogjekt for å bli sendt til metrikker
    
    * Sender mottatt ia-tjeneste til backend. Feil håndtering + begrense til 5 mislykkede forsøk
    
    * Deploy branch til dev-gcp og labs
    
    Co-authored-by: Malaz Alkoj <malaz.alkoj@nav.no>

---
+++ src/utils/ia-tjeneste-metrikker.ts
@@ -0,0 +1,53 @@
+export interface IatjenesteMetrikk {
+    type: String;
+    kilde: String;
+    tjenesteMottakkelsesdato: String;
+}
+
+
+const getIaTjenesterMetrikkerUrl = () => {
+    if (typeof window === "undefined") {
+        return 'http://localhost:8080/ia-tjenester-metrikker';
+    }
+
+    switch (window.location.hostname) {
+        case 'localhost':
+            return 'http://localhost:8080/ia-tjenester-metrikker';
+        case 'arbeidsgiver.nav.no':
+            return 'https://arbeidsgiver.nav.no/ia-tjenester-metrikker';
+        default:
+            return 'https://ia-tjenester-metrikker.dev.intern.nav.no';
+    }
+};
+
+const iaTjenesterMetrikkerAPI = `${getIaTjenesterMetrikkerUrl()}/uinnlogget/mottatt-iatjeneste`;
+
+export const tilIsoDatoMedUtcTimezoneUtenMillis = (dato: Date): String => {
+    return dato.toISOString().split('.')[0] + "Z";
+}
+
+export const sendIATjenesteMetrikk = async () => {
+    const iaTjenesteMetrikk: IatjenesteMetrikk = {
+        kilde: 'SYKEFRAVÆRSSTATISTIKK',
+        type: 'DIGITAL_IA_TJENESTE',
+        tjenesteMottakkelsesdato: tilIsoDatoMedUtcTimezoneUtenMillis(new Date()),
+    };
+
+    const settings = {
+        method: 'POST',
+        credentials: 'omit',
+        body: JSON.stringify(iaTjenesteMetrikk),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+    };
+    try {
+        // @ts-ignore
+        const fetchResponse = await fetch(`${iaTjenesterMetrikkerAPI}`, settings);
+        const data = await fetchResponse.json();
+        return data.status === 'created';
+    } catch (e) {
+        return false;
+    }
+};
