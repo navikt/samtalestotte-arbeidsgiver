0e9ae3a86c1c99a20e5d427210ddff8a3e1167cf
erlendaxelsson <erlend.axelsson@gmail.com>
2021-05-28 05:36:04 +0200

    Layout endring for små skjermer

--- src/felleskomponenter/EkspanderbartInfopanel/EkspanderbartInfopanelV2.tsx
+++ src/felleskomponenter/EkspanderbartInfopanel/EkspanderbartInfopanelV2.tsx
@@ -70,10 +70,13 @@ export const EkspanderbartInfopanel: FunctionComponent<EkspanderbartInfopanelPro
         <div className={"ekspanderbart-infopanel__root"}>
             <EkspanderbartpanelBase
                 tittel={
-                    <div className={"ekspanderbart-infopanel__tittel-grid"}>
-                        {props.ikon}
-                        <div>{props.tittel}</div>
-                        {erLest && <Lest width={"62px"} height={"24px"} />}
+                    <div className={"ekspanderbart-infopanel__tittel-flex-container"}>
+                        <div className={"ekspanderbart-infopanel__tittel-grid"}>
+                            {props.ikon}
+                            <div>{props.tittel}</div>
+                            {erLest && <Lest width={"62px"} height={"24px"} />}
+                        </div>
+                        <div className={"ekspanderbart-infopanel__tittel-apne"}>Åpne</div>
                     </div>
                 }
                 id={panelknappID}
