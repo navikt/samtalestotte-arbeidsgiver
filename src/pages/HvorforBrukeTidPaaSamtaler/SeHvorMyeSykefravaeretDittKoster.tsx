939abfd382900c165e5d0d04332578010ca89fa3
Malaz Alkoj <malaz.alkoj@nav.no>
2021-06-02 14:09:58 +0200

    lage og bruk LoggbarLenkepanelBase

--- src/pages/HvorforBrukeTidPaaSamtaler/SeHvorMyeSykefravaeretDittKoster.tsx
+++ src/pages/HvorforBrukeTidPaaSamtaler/SeHvorMyeSykefravaeretDittKoster.tsx
@@ -3,19 +3,32 @@ import Locked from '../../felleskomponenter/Ikoner/Locked';
 import Calculator from '../../felleskomponenter/Ikoner/Calculator';
 import './SeHvorMyeSykefravaeretDittKoster.less';
 import { MINSIDE_ARBEIDSGIVER } from '../../resources/urls';
-
+import LoggbarLenkepanelBase from '../../felleskomponenter/LoggbarLenkepanelBase/LoggbarLenkepanelBase';
 
 const SeHvorMyeSykefravaeretDittKoster = () => {
-    return <LenkepanelBase className={"lenkepanel-sykefravaer-kalkulator"} href={MINSIDE_ARBEIDSGIVER}>
-        <div className={"lenkepanel-sykefravaer-child-wrapper"}>
-            <Calculator className={"lenkepanel-sykefravaer-ikon"} height={"44px"} width={"44px"}/>
-            <p>Se hvor mye sykefraværet ditt koster</p>
-            <div>
-                <span>Gå til kalkulatoren</span>
-                <span className={"lenkepanel-sykefravaer-require-login"}><Locked/> Krever innlogging</span>
+    const lenkeTekstConst = 'Gå til kalkulatoren';
+    return (
+        <LoggbarLenkepanelBase
+            className={'lenkepanel-sykefravaer-kalkulator'}
+            href={MINSIDE_ARBEIDSGIVER}
+            lenketekst={lenkeTekstConst}
+        >
+            <div className={'lenkepanel-sykefravaer-child-wrapper'}>
+                <Calculator
+                    className={'lenkepanel-sykefravaer-ikon'}
+                    height={'44px'}
+                    width={'44px'}
+                />
+                <p>Se hvor mye sykefraværet ditt koster</p>
+                <div>
+                    <span>{lenkeTekstConst}</span>
+                    <span className={'lenkepanel-sykefravaer-require-login'}>
+                        <Locked /> Krever innlogging
+                    </span>
+                </div>
             </div>
-        </div>
-    </LenkepanelBase>
-}
+        </LoggbarLenkepanelBase>
+    );
+};
 
-export default SeHvorMyeSykefravaeretDittKoster
+export default SeHvorMyeSykefravaeretDittKoster;
