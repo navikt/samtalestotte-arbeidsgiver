8b0f3980e7032c6f8c26cece60d624373d25973f
erlendaxelsson <erlend.axelsson@gmail.com>
2021-06-10 07:28:06 +0200

    Gjøre at viste du at ikonet blir mindre pluss noen mindre stilendringer

--- src/pages/VisteDuAt.tsx
+++ src/pages/VisteDuAt.tsx
@@ -10,14 +10,14 @@ const VisteDuAt = () => (
         <h3 className={"viste-du-header"}>
             Visste du at NAV hjelper virksomheter med å forebygge sykefravær?
         </h3>
-        <Information className={"viste-du-icon"} width={"4rem"} height={"4rem"}/>
+        <Information className={"viste-du-icon"} width={"39px"} height={"39px"}/>
 
         <p className={"viste-du-paragraf"}>
             NAV hjelper virksomheter med å forebygge sykefravær.
             Du får digitale tjenester og veiledning for å gjennomføre enkeltsamtaler.
             NAV kan også gi mer omfattende rådgivning hos dere på arbeidsplassen.
         </p>
-        <LoggbarLenke className={"viste-du-link"} href={KONTAKT_OSS}>Kontakt NAV</LoggbarLenke>
+        <LoggbarLenke className={"viste-du-link margin-bunn-1rem"} href={KONTAKT_OSS}>Kontakt NAV</LoggbarLenke>
         <LoggbarLenke className={"viste-du-link"} href={FOREBYGGE_SYKEFRAVAER}>Les mer om NAVs tjenester for å forebygge sykefravær.</LoggbarLenke>
     </div>
 )
