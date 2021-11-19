350087f33fea2d28419505c5fae9878b20bb93af
Malaz Alkoj <malaz.alkoj@nav.no>
2021-11-19 15:46:14 +0100

    fix, doubleclick to hide white space, førte til at kun 1,2 sider blir vist når åpner filen

--- src/resources/textContent.ts
+++ src/resources/textContent.ts
@@ -23,7 +23,7 @@ import {
 
 const slikSkaperDuGodeSamtaler: (DocumentElement | object)[] = [
     { type: 'BigHeader', content: 'Slik skaper du gode samtaler' },
-    { type: 'MediumHeader', content: 'Last ned verktøyet og rediger selv:', isLessSpace: true },
+    { type: 'SmallHeader', content: 'Last ned verktøyet og rediger selv:', isLessSpace: true },
     { type: 'DownloadButtons' },
     {
         type: 'Paragraph',
