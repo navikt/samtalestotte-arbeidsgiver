8e8f960b1c5bd1c1b13d4619a7f43f43a586134e
runarskagestad <runar.skagestad@nav.no>
2022-01-26 15:48:54 +0100

    Byttet apiKey med "default", den skal ikke være nødvendig med collect-auto

--- src/amplitude/amplitude.ts
+++ src/amplitude/amplitude.ts
@@ -2,22 +2,16 @@ import amplitude from 'amplitude-js';
 
 let initiated = false;
 
-const getApiKey = () => {
-    return window.location.hostname === 'arbeidsgiver.nav.no'
-        ? '3a6fe32c3457e77ce81c356bb14ca886'
-        : '55477baea93c5227d8c0f6b813653615';
-};
-
 export default function logEvent(eventName: string, data?: any): Promise<any> {
-
     if (!initiated) {
-        amplitude.getInstance().init(getApiKey(), '', {
+        const amplitudeConfig = {
             apiEndpoint: 'amplitude.nav.no/collect-auto',
             saveEvents: false,
             includeUtm: true,
-            batchEvents: false,
             includeReferrer: true,
-        });
+            platform: window.location.toString(),
+        };
+        amplitude.getInstance().init('default', undefined, amplitudeConfig);
         initiated = true;
     }
     return new Promise(function (resolve) {
