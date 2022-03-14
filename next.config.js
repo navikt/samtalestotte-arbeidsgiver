a85bc0e09670564959742d7165ea028efc05e186
Thomas Dufourd <thomas.dufourd@gmail.com>
2022-03-14 10:31:20 +0100

    Innføre csp (#200)
    * Legge til Content-Security-Policy header på alle req/res
    
    * Deploy i Labs og dev miljø
    
    * Legge til url til tjeneste som brukes Task Analytics (behov fra dekoratør, tvers av nav.no)
    
    * Legge til andre security headers: X-Frame-Options, X-Content-Type-Options, Referrer-Policy og Permissions-Policy

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@
  **/
 
 const nextTranspileModules = require('next-transpile-modules');
-const withTranspileModules = nextTranspileModules(["@navikt/ds-react", "@navikt/ds-icons"]);
+const withTranspileModules = nextTranspileModules(['@navikt/ds-react', '@navikt/ds-icons']);
 const withLinaria = require('next-linaria');
+const csp = require('./src/csp');
 
 module.exports = withTranspileModules(
-    withLinaria(
-    {
+    withLinaria({
         basePath: '/samtalestotte',
         i18n: {
             locales: ['no'],
@@ -19,6 +19,40 @@ module.exports = withTranspileModules(
         trailingSlash: false,
         reactStrictMode: true,
         webpack5: true,
-        cssModules: true
-    }
-));
+        cssModules: true,
+        async headers() {
+            return [
+                {
+                    // Apply these headers to all routes in your application.
+                    source: '/:path*',
+                    headers: [
+                        {
+                            key: 'X-Frame-Options',
+                            value: 'SAMEORIGIN',
+                        },
+                        {
+                            key: 'X-Xss-Protection',
+                            value: '1; mode=block',
+                        },
+                        {
+                            key: 'X-Content-Type-Options',
+                            value: 'nosniff',
+                        },
+                        {
+                            key: 'Referrer-Policy',
+                            value: 'no-referrer',
+                        },
+                        {
+                            key: 'Permissions-Policy',
+                            value: 'geolocation=(), microphone=(), camera=()',
+                        },
+                        {
+                            key: 'Content-Security-Policy',
+                            value: csp,
+                        },
+                    ],
+                },
+            ];
+        },
+    })
+);
