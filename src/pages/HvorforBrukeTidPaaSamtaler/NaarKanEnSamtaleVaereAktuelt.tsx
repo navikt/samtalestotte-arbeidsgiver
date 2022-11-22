cceb8512f21a14b0e97810138361f760f43d0714
runarskagestad <runar.skagestad@nav.no>
2022-11-22 16:38:34 +0100

    ul -> Liste

--- src/pages/HvorforBrukeTidPaaSamtaler/NaarKanEnSamtaleVaereAktuelt.tsx
+++ src/pages/HvorforBrukeTidPaaSamtaler/NaarKanEnSamtaleVaereAktuelt.tsx
@@ -2,41 +2,43 @@ import { EkspanderbartInfopanel } from '../../felleskomponenter/EkspanderbartInf
 import InfoPanelProps from '../../felleskomponenter/InfoPanel/InfoPanelProps';
 import ChatDoubleBubble from '../../felleskomponenter/Ikoner/ChatDoubleBubble';
 import fellesStiler from '../../utils/fellesStiler.module.css';
-import classNames from "classnames";
-
+import classNames from 'classnames';
+import { Liste } from '../../Liste/Liste';
 
 const NaarKanEnSamtaleVaereAktuelt = (props: InfoPanelProps) => {
-    return <EkspanderbartInfopanel
-        unikId={'naarKanEnsamtaleVaereAktuelt'}
-        tittel={'Når kan en samtale være aktuelt?'}
-        panelLestSituasjon={'ulest'}
-        ikon={<ChatDoubleBubble width={"44px"} height={"44px"}/>}
-        lestIkon={<ChatDoubleBubble width={"44px"} height={"44px"}/>}
-    >
-        <div className={fellesStiler.marginTop1Rem}>
-        <p className={classNames(fellesStiler.marginTop0Rem, fellesStiler.marginBottom0Rem)}>
-            <strong>En samtale for å forebygge eller følge opp sykefravær handler om arbeidssituasjonen.</strong>
-        </p>
-        <p className={fellesStiler.marginTop0Rem}>
-            En samtale bør gjennomføres når
-        </p>
-        <ul>
-            <li>
-                Du eller din medarbeider opplever utfordringer med arbeidet og det skyldes sykdom eller andre forhold.
-            </li>
-            <li>
-                Medarbeideren står i fare for å bli sykmeldt.
-            </li>
-            <li>
-                Medarbeideren er sykmeldt.
-            </li>
-            <li>
-                Medarbeideren har vært sykmeldt.
-            </li>
-
-        </ul>
-        </div>
-    </EkspanderbartInfopanel>;
+    return (
+        <EkspanderbartInfopanel
+            unikId={'naarKanEnsamtaleVaereAktuelt'}
+            tittel={'Når kan en samtale være aktuelt?'}
+            panelLestSituasjon={'ulest'}
+            ikon={<ChatDoubleBubble width={'44px'} height={'44px'} />}
+            lestIkon={<ChatDoubleBubble width={'44px'} height={'44px'} />}
+        >
+            <div className={fellesStiler.marginTop1Rem}>
+                <p
+                    className={classNames(
+                        fellesStiler.marginTop0Rem,
+                        fellesStiler.marginBottom0Rem
+                    )}
+                >
+                    <strong>
+                        En samtale for å forebygge eller følge opp sykefravær handler om
+                        arbeidssituasjonen.
+                    </strong>
+                </p>
+                <p className={fellesStiler.marginTop1Rem}>En samtale bør gjennomføres når</p>
+                <Liste>
+                    <Liste.Element>
+                        Du eller din medarbeider opplever utfordringer med arbeidet og det skyldes
+                        sykdom eller andre forhold.
+                    </Liste.Element>
+                    <Liste.Element>Medarbeideren står i fare for å bli sykmeldt.</Liste.Element>
+                    <Liste.Element>Medarbeideren er sykmeldt.</Liste.Element>
+                    <Liste.Element>Medarbeideren har vært sykmeldt.</Liste.Element>
+                </Liste>
+            </div>
+        </EkspanderbartInfopanel>
+    );
 };
 
 export default NaarKanEnSamtaleVaereAktuelt;
--- src/pages/MerInspirasjonOgGodeGrep/PsykiskHelsePåArbeidsplassen.tsx
+++ src/pages/MerInspirasjonOgGodeGrep/PsykiskHelsePåArbeidsplassen.tsx
@@ -1,12 +1,14 @@
-import {EkspanderbartInfopanel} from '../../felleskomponenter/EkspanderbartInfopanel/EkspanderbartInfopanel';
+import { EkspanderbartInfopanel } from '../../felleskomponenter/EkspanderbartInfopanel/EkspanderbartInfopanel';
 import fellesStiler from '../../utils/fellesStiler.module.css';
 import { Heading } from '@navikt/ds-react';
 import LoggbarLenke from '../../felleskomponenter/LoggbarLenke/LoggbarLenke';
 import {
-    FILM_OM_PSYKISK_HELSE, IDEBANKEN_PSYKISK_HELSE,
     ARBEIDSGIVERLOS_ANSATTE_SOM_SLITTER_PSYKISK,
+    IDEBANKEN_PSYKISK_HELSE,
 } from '../../resources/urls';
 import classNames from 'classnames';
+import { Liste } from '../../Liste/Liste';
+import { InfoBoks } from '../../felleskomponenter/InfoBoks/InfoBoks';
 
 const PsykiskHelsePåArbeidsplassen = () => {
     return (
@@ -25,83 +27,91 @@ const PsykiskHelsePåArbeidsplassen = () => {
                     Endringene kan komme til uttrykk på forskjellige måter.
                 </p>
                 <p className={fellesStiler.marginTop1Rem}>Mulige konsekvenser på jobb kan være:</p>
-                <ul className={fellesStiler.marginTop0Rem}>
-                    <li>Problemer med å forstå og huske informasjon</li>
-                    <li>Utfordringer med å gjøre flere oppgaver samtidig</li>
-                    <li>Vanskeligheter med å prioritere og ferdigstille oppgaver</li>
-                    <li>Problemer med å beregne tid</li>
-                    <li>Virker sliten, og slutter å ta initiativ</li>
-                    <li>Virker motløs og irritabel, og kan oppleves vanskelig</li>
-                    <li>Konsentrasjonsproblemer eller kan virker fraværende</li>
-                    <li>Utfordringer med å samarbeide og forholde seg til andre mennesker</li>
-                    <li>
+                <Liste className={fellesStiler.marginTop0Rem}>
+                    <Liste.Element>Problemer med å forstå og huske informasjon</Liste.Element>
+                    <Liste.Element>Utfordringer med å gjøre flere oppgaver samtidig</Liste.Element>
+                    <Liste.Element>
+                        Vanskeligheter med å prioritere og ferdigstille oppgaver
+                    </Liste.Element>
+                    <Liste.Element>Problemer med å beregne tid</Liste.Element>
+                    <Liste.Element>Virker sliten, og slutter å ta initiativ</Liste.Element>
+                    <Liste.Element>
+                        Virker motløs og irritabel, og kan oppleves vanskelig
+                    </Liste.Element>
+                    <Liste.Element>
+                        Konsentrasjonsproblemer eller kan virker fraværende
+                    </Liste.Element>
+                    <Liste.Element>
+                        Utfordringer med å samarbeide og forholde seg til andre mennesker
+                    </Liste.Element>
+                    <Liste.Element>
                         Trekker seg unna sosiale aktiviteter, orker ikke å være sammen med kollegaer
-                    </li>
-                </ul>
-
-                <p className={fellesStiler.marginTop3Rem}>Alle kan ha en dårlig dag, og det er lov.</p>
-
-                <ul className={fellesStiler.marginTop0Rem}>
-                    <li>
+                    </Liste.Element>
+                </Liste>
+                <p className={fellesStiler.marginTop3Rem}>
+                    Alle kan ha en dårlig dag, og det er lov.
+                </p>
+                <Liste>
+                    <Liste.Element>
                         Hvor mange dårlige dager skal en medarbeider ha før du tar initiativ til en
                         samtale?
-                    </li>
-                    <li>
+                    </Liste.Element>
+                    <Liste.Element>
                         Hvordan møter du som leder og de øvrige kollegaene en medarbeider med
                         utfordringer?
-                    </li>
-                </ul>
+                    </Liste.Element>
+                </Liste>
 
                 <Heading size="small" level={'4'} className={fellesStiler.marginTop3Rem}>
                     Dette kan du gjøre:
                 </Heading>
 
-                <div className={classNames(fellesStiler.infoPanelKolonner, fellesStiler.marginTop1Rem)}>
-                    <div className={fellesStiler.graAvrundetBoks}>
-                        <Heading size={'small'} level={'4'}>
-                            på kort sikt:
-                        </Heading>
-                        <ul className={fellesStiler.marginTop0Rem}>
-                            <li>
+                <div
+                    className={classNames(
+                        fellesStiler.infoPanelKolonner,
+                        fellesStiler.marginTop1Rem
+                    )}
+                >
+                    <InfoBoks overskrift="på kort sikt:">
+                        <Liste>
+                            <Liste.Element>
                                 Vis omsorg, spør hvordan noen har det, lytt og still krav.
-                            </li>
-                            <li>
+                            </Liste.Element>
+                            <Liste.Element>
                                 Gjennomfør flere korte samtaler. Husk: positive tilbakemeldinger og
                                 skriftlighet.
-                            </li>
-                            <li>
+                            </Liste.Element>
+                            <Liste.Element>
                                 Bruk veiledningen i «Slik skaper du gode samtaler» (lenke til egen
                                 side) til å forberede og gjennomføre en samtale om arbeid.
-                            </li>
-                            <li>
+                            </Liste.Element>
+                            <Liste.Element>
                                 Du kan be om veiledning i forkant av samtalen hvis du har behov.
                                 HR-/personalavdeling, bedriftshelsetjenesten eller NAV har erfaring
                                 og kan hjelpe deg.
-                            </li>
-                            <li>
+                            </Liste.Element>
+                            <Liste.Element>
                                 <LoggbarLenke href={ARBEIDSGIVERLOS_ANSATTE_SOM_SLITTER_PSYKISK}>
-                                    Les mer om hjelp du kan få fra NAVs arbeidsgiverlos hvis en ansatt sliter psykisk
+                                    Les mer om hjelp du kan få fra NAVs arbeidsgiverlos hvis en
+                                    ansatt sliter psykisk
                                 </LoggbarLenke>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className={fellesStiler.graAvrundetBoks}>
-                        <Heading size={'small'} level={'4'}>
-                            over tid og som kontinuerlig arbeid:
-                        </Heading>
-                        <ul className={fellesStiler.marginTop0Rem}>
-                            <li className={fellesStiler.marginTop1Rem}>
+                            </Liste.Element>
+                        </Liste>
+                    </InfoBoks>
+                    <InfoBoks overskrift="over tid og som kontinuerlig arbeid:">
+                        <Liste>
+                            <Liste.Element>
                                 Se etter tegn fra lista over og ta initiativ til en samtale før et
                                 sykefravær oppstår.
-                            </li>
-                            <li>
+                            </Liste.Element>
+                            <Liste.Element>
                                 <LoggbarLenke href={IDEBANKEN_PSYKISK_HELSE}>
                                     Les mer om "Slik kan du bidra til bedre psykisk helse på
                                     arbeidsplassen" hos idebanken.
                                 </LoggbarLenke>
-                            </li>
-                        </ul>
-                    </div>
+                            </Liste.Element>
+                        </Liste>
+                    </InfoBoks>
                 </div>
             </div>
         </EkspanderbartInfopanel>
