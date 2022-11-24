2349ba28def98d45f6964e9da91b649cc0d7b84d
runarskagestad <runar.skagestad@nav.no>
2022-11-24 12:45:24 +0100

    Fikset testContent

--- __tests__/resources/textContent.test.ts
+++ __tests__/resources/textContent.test.ts
@@ -1,55 +1,78 @@
 import {
     BigHeader,
     Columns,
-    DocumentElement, HorizontalLine, InfoBox, isBigHeader,
-    isColumns, isDownloadButtons, isHorizontalLine,
-    isInfoBox, isLink,
-    isList, isMediumHeader,
+    DocumentElement,
+    HorizontalLine,
+    InfoBox,
+    isBigHeader,
+    isColumns,
+    isDownloadButtons,
+    isHorizontalLine,
+    isInfoBox,
+    isLink,
+    isList,
+    isMediumHeader,
     isPanel,
-    isParagraph, isSmallHeader, isText, Link, List, MediumHeader, Panel, Paragraph, SmallHeader, Text
-} from "../../src/dokumentgenerator/domainInterfaces";
-import {isBoolean, isNumber, isString} from "../../src/utils/typeGuardUtils";
-import {SLIK_SKAPER_DU_GODE_SAMTALER_CONTENT} from "../../src/resources/textContent";
+    isParagraph,
+    isSmallHeader,
+    isSpan,
+    isText,
+    Link,
+    List,
+    MediumHeader,
+    Panel,
+    Paragraph,
+    SmallHeader,
+    Span,
+    Text,
+} from '../../src/dokumentgenerator/domainInterfaces';
+import { isBoolean, isNumber, isString } from '../../src/utils/typeGuardUtils';
+import { SLIK_SKAPER_DU_GODE_SAMTALER_CONTENT } from '../../src/resources/textContent';
 
-test("Validerer JSON", () => {
-    expect(isValidDomainObjects(SLIK_SKAPER_DU_GODE_SAMTALER_CONTENT)).toBe(true)
-})
+test('Validerer JSON', () => {
+    expect(isValidDomainObjects(SLIK_SKAPER_DU_GODE_SAMTALER_CONTENT)).toBe(true);
+});
 
-const isValidDomainObjects = (objects: (object | DocumentElement)[]): objects is DocumentElement[] => {
-    return objects.every(e => {
-        if(isParagraph(e)){
+const isValidDomainObjects = (
+    objects: (object | DocumentElement)[]
+): objects is DocumentElement[] => {
+    return objects.every((e) => {
+        if (isParagraph(e)) {
             return isValidParagraph(e);
         }
-        if(isList(e)){
+        if (isSpan(e)) {
+            return isValidSpan(e);
+        }
+        if (isList(e)) {
             return isValidList(e);
         }
-        if(isPanel(e)){
+        if (isPanel(e)) {
             return isValidPanel(e);
         }
-        if(isInfoBox(e)){
+        if (isInfoBox(e)) {
             return isValidInfoBox(e);
         }
-        if(isColumns(e)){
+        if (isColumns(e)) {
             return isValidColumns(e);
         }
-        if(isSmallHeader(e)){
+        if (isSmallHeader(e)) {
             return isValidSmallHeader(e);
         }
-        if(isMediumHeader(e)){
+        if (isMediumHeader(e)) {
             return isValidMediumHeader(e);
         }
-        if(isBigHeader(e)){
+        if (isBigHeader(e)) {
             return isValidBigHeader(e);
         }
-        if(isHorizontalLine(e)){
+        if (isHorizontalLine(e)) {
             return true;
         }
-        if(isDownloadButtons(e)){
+        if (isDownloadButtons(e)) {
             return true;
         }
         return false;
-    })
-}
+    });
+};
 
 const isValidText = (text: Text): text is Text => {
     if((isBoolean(text.bold) || text.bold === undefined) && isString(text.content) && (isNumber(text.lineBreak) || text.lineBreak === undefined)){
@@ -67,44 +90,65 @@ const isValidLink = (link: Link): link is Link => {
 }
 
 const isValidParagraph = (paragraph: Paragraph): paragraph is Paragraph => {
-    if(isString(paragraph.content)) return true
-    if(
-        paragraph.content.every(e => {
-            if(isString(e)) return true
-            if(isText(e)) return isValidText(e)
-            if(isLink(e)) return isValidLink(e)
-            return false
+    if (isString(paragraph.content)) return true;
+    if (
+        paragraph.content.every((e) => {
+            if (isString(e)) return true;
+            if (isText(e)) return isValidText(e);
+            if (isLink(e)) return isValidLink(e);
+            return false;
         })
     ) {
-        return true
+        return true;
     }
-    throw new Error(`${paragraph} is not a valid Paragraph object`)
-}
+    throw new Error(`${paragraph} is not a valid Paragraph object`);
+};
+
+const isValidSpan = (span: Span): span is Span => {
+    if (isString(span.content)) return true;
+    if (
+        span.content.every((e) => {
+            if (isString(e)) return true;
+            if (isText(e)) return isValidText(e);
+            if (isLink(e)) return isValidLink(e);
+            return false;
+        })
+    ) {
+        return true;
+    }
+    throw new Error(`${span} is not a valid Span object`);
+};
 
 const isValidList = (list: List): list is List => {
-     if(
-         isNumber(list.level) || list.level === undefined
-         && list.content.every(sublist => sublist.every(e => {
-             if(isString(e)) return true
-             if(isText(e)) return isValidText(e)
-             if(isLink(e)) return isValidLink(e)
-             if(isParagraph(e)) return isValidParagraph(e)
-             if(isSmallHeader(e)) return isValidSmallHeader(e)
-             if(isMediumHeader(e)) return isValidMediumHeader(e)
-             if(isBigHeader(e)) return isValidBigHeader(e)
-             if(isList(e)) return isValidList(e)
-             return false
-        }))
-     ){
-         return true
-     }
+    if (
+        isNumber(list.level) ||
+        (list.level === undefined &&
+            list.content.every((sublist) =>
+                sublist.every((e) => {
+                    if (isString(e)) return true;
+                    if (isText(e)) return isValidText(e);
+                    if (isLink(e)) return isValidLink(e);
+                    if (isParagraph(e)) return isValidParagraph(e);
+                    if (isSpan(e)) return isValidSpan(e);
+                    if (isSmallHeader(e)) return isValidSmallHeader(e);
+                    if (isMediumHeader(e)) return isValidMediumHeader(e);
+                    if (isBigHeader(e)) return isValidBigHeader(e);
+                    if (isList(e)) return isValidList(e);
+                    return false;
+                })
+            ))
+    ) {
+        return true;
+    }
     throw new Error(`
-    ${list.content.flat().map(e=>{
-        if(isString(e)){ return e}
-        return e.type
+    ${list.content.flat().map((e) => {
+        if (isString(e)) {
+            return e;
+        }
+        return e.type;
     })} is not a valid List object
-    `)
-}
+    `);
+};
 
 const isValidPanel = (panel: Panel): panel is Panel => {
      if(
@@ -159,9 +203,10 @@ const isValidBigHeader = (bigHeader: BigHeader): bigHeader is BigHeader => {
 
 const validatePanelContent = (e: Paragraph | List | HorizontalLine | SmallHeader | MediumHeader | BigHeader | InfoBox | Columns) =>{
     if(isColumns(e)) return isValidColumns(e)
-    if(isInfoBox(e)) return isValidInfoBox(e)
-    if(isParagraph(e)) return isValidParagraph(e)
-    if(isList(e)) return isValidList(e)
+    if (isInfoBox(e)) return isValidInfoBox(e);
+    if (isParagraph(e)) return isValidParagraph(e);
+    if (isSpan(e)) return isValidSpan(e);
+    if (isList(e)) return isValidList(e)
     if(isHorizontalLine(e)) return true
     if(isSmallHeader(e)) return isValidSmallHeader(e)
     if(isMediumHeader(e)) return isValidMediumHeader(e)
@@ -169,12 +214,15 @@ const validatePanelContent = (e: Paragraph | List | HorizontalLine | SmallHeader
     return false
 }
 
-const validateTableContent = (e: Paragraph | List | HorizontalLine | SmallHeader | MediumHeader | BigHeader) =>{
-    if(isParagraph(e)) return isValidParagraph(e)
-    if(isList(e)) return isValidList(e)
-    if(isHorizontalLine(e)) return true
-    if(isSmallHeader(e)) return isValidSmallHeader(e)
-    if(isMediumHeader(e)) return isValidMediumHeader(e)
-    if(isBigHeader(e)) return isValidBigHeader(e)
-    return false
-}
+const validateTableContent = (
+    e: Paragraph | List | HorizontalLine | SmallHeader | MediumHeader | BigHeader
+) => {
+    if (isParagraph(e)) return isValidParagraph(e);
+    if (isSpan(e)) return isValidSpan(e);
+    if (isList(e)) return isValidList(e);
+    if (isHorizontalLine(e)) return true;
+    if (isSmallHeader(e)) return isValidSmallHeader(e);
+    if (isMediumHeader(e)) return isValidMediumHeader(e);
+    if (isBigHeader(e)) return isValidBigHeader(e);
+    return false;
+};
