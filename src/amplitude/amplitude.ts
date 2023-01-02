01e876dc21d062f529af6b0ebdb3f439594de903
runarskagestad <runar.skagestad@nav.no>
2023-01-02 11:45:26 +0100

    change amplitude project

--- src/amplitude/amplitude.ts
+++ src/amplitude/amplitude.ts
@@ -2,32 +2,19 @@ import amplitude from 'amplitude-js';
 
 let initiated = false;
 
-const AmplitudeBucket = {
-    ARBEIDSGIVER_PROD: 'a8243d37808422b4c768d31c88a22ef4',
-    ARBEIDSGIVER_DEV: '6ed1f00aabc6ced4fd6fcb7fcdc01b30',
-};
-
-const bucketId = () => {};
-
 export default function logEvent(eventName: string, data?: any): Promise<any> {
     if (!initiated) {
-        const bucketId =
-            window.location.hostname === 'arbeidsgiver.nav.no'
-                ? AmplitudeBucket.ARBEIDSGIVER_PROD
-                : AmplitudeBucket.ARBEIDSGIVER_DEV;
-
-        amplitude.getInstance().init(bucketId, '', {
-            apiEndpoint: 'amplitude.nav.no/collect',
+        amplitude.getInstance().init('default', '', {
+            apiEndpoint: 'amplitude.nav.no/collect-auto',
             saveEvents: false,
             includeUtm: true,
-            batchEvents: false,
             includeReferrer: true,
+            platform: window.location.toString(),
         });
-        initiated = true;
     }
     return new Promise(function (resolve) {
         amplitude
             .getInstance()
-            .logEvent(eventName, { app: 'samtalestøtte-arbeidsgiver', ...data }, resolve);
+            .logEvent(eventName, {app: 'samtalestøtte-arbeidsgiver', ...data}, resolve);
     });
 }
