5b95164cfc7f68ecd96b8855e61e9207b9c33359
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-06-16 12:15:04 +0200

    Redirect gamle docx lenker til hovedsiden

--- next.config.js
+++ next.config.js
@@ -5,55 +5,64 @@
 const csp = require('./src/csp');
 
 module.exports = {
-        transpilePackages: ['@navikt/ds-react', '@navikt/ds-icons'],
-        basePath: '/samtalestotte',
-        i18n: {
-            locales: ['no'],
-            defaultLocale: 'no',
-        },
-        trailingSlash: false,
-        reactStrictMode: true,
-        async headers() {
-            return [
-                {
-                    // Apply these headers to all routes in your application.
-                    source: '/:path*',
-                    headers: [
-                        {
-                            key: 'X-Frame-Options',
-                            value: 'SAMEORIGIN',
-                        },
-                        {
-                            key: 'X-Xss-Protection',
-                            value: '1; mode=block',
-                        },
-                        {
-                            key: 'X-Content-Type-Options',
-                            value: 'nosniff',
-                        },
-                        {
-                            key: 'Referrer-Policy',
-                            value: 'no-referrer',
-                        },
-                        {
-                            key: 'Permissions-Policy',
-                            value: 'geolocation=(), microphone=(), camera=()',
-                        },
-                        {
-                            key: 'Content-Security-Policy',
-                            value: csp,
-                        },
-                    ],
-                },
-                {
-                    source: '/dokumenter/:path*',
-                    headers: [
-                        {
-                            key: 'X-Robots-Tag',
-                            value: 'noindex'
-                        }
-                    ]
-                }
-            ];
-        },
+    transpilePackages: ['@navikt/ds-react', '@navikt/ds-icons'],
+    basePath: '/samtalestotte',
+    i18n: {
+        locales: ['no'],
+        defaultLocale: 'no',
+    },
+    trailingSlash: false,
+    reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: '/:path(^[^\\/]+\\.docx$)',
+                destination: '/',
+                permanent: true,
+            },
+        ];
+    },
+    async headers() {
+        return [
+            {
+                // Apply these headers to all routes in your application.
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                    {
+                        key: 'X-Xss-Protection',
+                        value: '1; mode=block',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'no-referrer',
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'geolocation=(), microphone=(), camera=()',
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: csp,
+                    },
+                ],
+            },
+            {
+                source: '/dokumenter/:path*',
+                headers: [
+                    {
+                        key: 'X-Robots-Tag',
+                        value: 'noindex',
+                    },
+                ],
+            },
+        ];
+    },
 };
