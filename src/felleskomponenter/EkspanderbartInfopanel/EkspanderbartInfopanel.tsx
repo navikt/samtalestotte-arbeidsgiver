b07ba1775c10e1493d386280de93f969d7f514f9
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-07-11 10:39:12 +0200

    Fjernet ikon fra EkspanderbartInfopanel. Puttet "lest" boks helt til høyre i infopanel.

--- src/felleskomponenter/EkspanderbartInfopanel/EkspanderbartInfopanel.tsx
+++ src/felleskomponenter/EkspanderbartInfopanel/EkspanderbartInfopanel.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent, ReactNode, useEffect, useRef, useState } from 'react';
-import { Accordion } from '@navikt/ds-react';
+import { Accordion, Heading } from '@navikt/ds-react';
 import { Expand } from '@navikt/ds-icons';
 import classNames from 'classnames';
 import Lest from '../Ikoner/Lest';
@@ -16,8 +16,6 @@ export interface EkspanderbartInfopanelProps {
     tittel: string;
     bakgrunn?: string;
     panelLestSituasjon: PanelLestSituasjon;
-    ikon?: ReactNode;
-    lestIkon?: ReactNode;
 }
 
 export const EkspanderbartInfopanel: FunctionComponent<EkspanderbartInfopanelProps> = (
@@ -29,8 +27,6 @@ export const EkspanderbartInfopanel: FunctionComponent<EkspanderbartInfopanelPro
     const [hovedMeny, setHovedMeny] = useState<HTMLElement | null>(null);
 
     const panelknappID = 'ekspanderbart-infopanel__' + props.unikId + '-base';
-
-    const hasIcon = props.ikon !== null && props.ikon !== undefined;
 
     const accordionRef = useRef<HTMLButtonElement>(null);
 
@@ -77,19 +73,10 @@ export const EkspanderbartInfopanel: FunctionComponent<EkspanderbartInfopanelPro
                         }}
                         className={classNames(styles.panel, { [styles.borderBottom]: erÅpen })}
                     >
-                        <div className={classNames(styles.flexContainer)}>
-                            <div
-                                className={classNames(styles.grid, {
-                                    [styles.gridWithIcon]: hasIcon,
-                                })}
-                            >
-                                {hasIcon && (
-                                    <div className={styles.ikonContainer}>{props.ikon}</div>
-                                )}
-                                <div className={styles.tittelTekst}>{props.tittel}</div>
-                                {erLest && <Lest width={'62px'} height={'24px'} />}
-                            </div>
-                        </div>
+                        <Heading size="medium" level="3" className={styles.heading}>
+                            <div className={styles.tittelTekst}>{props.tittel}</div>
+                            {erLest && <Lest width={'62px'} height={'24px'} />}
+                        </Heading>
                     </Accordion.Header>
 
                     <Accordion.Content className={styles.panelInnhold}>
