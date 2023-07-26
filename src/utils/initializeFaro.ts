77f999d48407b31db92dd2d313838548feca9c75
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-07-26 10:39:05 +0200

    Fjernet logging av grafana url.

--- src/utils/initializeFaro.ts
+++ src/utils/initializeFaro.ts
@@ -2,7 +2,6 @@ import { faro, initializeFaro, getWebInstrumentations } from '@grafana/faro-web-
 
 export function doInitializeFaro(grafanaAgentUrl: string) {
     if (Object.keys(faro).length === 0 && grafanaAgentUrl?.length > 0) {
-        console.log('grafanaAgentUrl :>> ', grafanaAgentUrl);
         initializeFaro({
             url: grafanaAgentUrl,
             app: {
