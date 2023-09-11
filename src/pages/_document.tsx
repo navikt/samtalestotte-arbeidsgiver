60a5861bd6a30492b2ea4ff2689123b6519ba397
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-09-11 11:48:41 +0200

    Oppgraderte nav-dekoratøren-modular La til logoutWarning som parameter.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,34 +1,53 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
-import { Components, Env, fetchDecoratorReact, Props } from '@navikt/nav-dekoratoren-moduler/ssr';
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+    DocumentProps,
+} from 'next/document';
+import {
+    fetchDecoratorReact,
+    DecoratorEnvProps,
+    DecoratorParams,
+    DecoratorComponents,
+} from '@navikt/nav-dekoratoren-moduler/ssr';
 import React from 'react';
 import { getBreadcrumbs } from '../utils/innloggetStatus';
 import { getUrlsFromEnv } from '../utils/envUtils';
 
 const getDecoratorEnv = () =>
-    process.env.NAIS_CLUSTER_NAME === 'prod-gcp' ? 'prod' : ('dev' as Exclude<Env, 'localhost'>);
+    process.env.NAIS_CLUSTER_NAME === 'prod-gcp' ? 'prod' : ('dev' as DecoratorEnvProps['env']);
 
-const getDecoratorParams = (environment: Env, breadcrumbs: Props['breadcrumbs']): Props => ({
-    env: environment,
+const getDecoratorParams = (
+    breadcrumbs: DecoratorParams['breadcrumbs']
+): Partial<DecoratorParams> => ({
     context: 'arbeidsgiver',
     breadcrumbs: breadcrumbs,
+    logoutWarning: true,
 });
 
-class _document extends Document<{ decorator: Components }> {
+class _document extends Document<DocumentProps & { Decorator: DecoratorComponents }> {
     static async getInitialProps(ctx: DocumentContext) {
         const initialProps = await Document.getInitialProps(ctx);
 
         const decoratorEnv = getDecoratorEnv();
         const envUrls = getUrlsFromEnv();
         const breadcrumbs = getBreadcrumbs(envUrls);
-        const decoratorParams = getDecoratorParams(decoratorEnv, breadcrumbs);
 
-        const decorator = await fetchDecoratorReact(decoratorParams);
+        const Decorator = await fetchDecoratorReact({
+            env: decoratorEnv,
+            localUrl: '',
+            serviceDiscovery: true,
+            params: getDecoratorParams(breadcrumbs),
+        });
 
-        return { ...initialProps, decorator };
+        return { ...initialProps, Decorator };
     }
 
     render() {
-        const { Styles, Scripts, Header, Footer } = this.props.decorator;
+        const { Decorator } = this.props;
+        const { Styles, Scripts, Header, Footer } = Decorator;
         return (
             <Html>
                 <Head>
